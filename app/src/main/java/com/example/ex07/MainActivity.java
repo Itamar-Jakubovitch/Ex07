a6c2e4ba20fe5f8592b1ca2c7c62fa0ba94e1fee
itamar_jakubovitch <Itamarjakubovitch@gmail.com>
2026-01-12 22:41:56 +0200

    changing the photos

--- app/src/main/java/com/example/ex07/MainActivity.java
+++ app/src/main/java/com/example/ex07/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex07;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView iV;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
